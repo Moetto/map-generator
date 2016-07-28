474b76188417fd6294093188840ab6a2a22885c3
Antti Möttönen <antti.moettoenen@gmail.com>
2016-07-28 19:51:12 +0300

    Parameterize scales better

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 * Use morphology to generate more natural / interesting shorelines for example
 * Improve performance
 * Create GUI to make faster changes
+* Simulate continents
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from noise import snoise2
 import numpy
-from random import randint
+import random
 from PIL import Image
 import configargparse
 import re
@@ -21,30 +21,46 @@ def check_hex(value):
     return "#" + value
 
 
-class Map:
-    SEA = " "
-    SHORE = "~"
-    LAND = "x"
-    EMPTY = " "
+def scale_pair(value):
+    values = value.split(',')
+    pairs = []
+    for value in values:
+        pairs.append(_ScaleEffectPair.parse_pair(value))
+    return pairs
 
-    def __init__(self, size_x=50, size_y=50, seed=10, scale=100, scale2=50):
+
+class _ScaleEffectPair:
+    def __init__(self, scale, effect):
+        self.scale = int(scale)
+        self.effect = float(effect)
+
+    @staticmethod
+    def parse_pair(value):
+        scale, effect = value.split("/")
+        return _ScaleEffectPair(scale, effect)
+
+    def __str__(self):
+        return "{} / {}".format(self.scale, self.effect)
+
+
+class Map:
+    def __init__(self, size_x, size_y, filters, seed=10):
         self.size_x = size_x
         self.size_y = size_y
         self.grid = numpy.zeros((size_y, size_x), numpy.float32)
+        self.max_height = sum([pair.effect for pair in filters]) * 2
+
+        random.seed(seed)
+        offset = random.randint(0, 10000)
         for x in range(size_x):
             for y in range(size_y):
-                height = snoise2((x + seed) / scale, (y + seed) / scale, 1) + 1.1 + \
-                         snoise2((x + seed) / scale2, (y + seed) / scale2, 1) / 10
-                # self.grid[y][x] = self.LAND if height > sea_height else self.SEA
+                height = self.max_height / 2
+                for pair in filters:
+                    height += snoise2((x + offset) / pair.scale, (y + offset) / pair.scale, 1) * pair.effect
                 self.grid[y][x] = height
 
-    def print_self(self):
-        print(" " + "".join(["-" for i in range(self.size_x)]) + " ")
-        for row in self.grid:
-            print("|" + "".join(row) + "|")
-        print(" " + "".join(["-" for i in range(self.size_x)]) + " ")
-
-    def _calculate_gradient_value(self, value, start_value, end_value, start_rgb, end_rgb, channel):
+    @staticmethod
+    def _calculate_gradient_value(value, start_value, end_value, start_rgb, end_rgb, channel):
         percentage = (value - start_value) / (end_value - start_value)
         return int(start_rgb[channel] * (1 - percentage) + end_rgb[channel] * percentage)
 
@@ -63,7 +79,7 @@ class Map:
                     val = self.grid[y][x]
                     if val > sea_level:
                         start_value = sea_level
-                        end_value = 2.2
+                        end_value = self.max_height
                         start_rgb = ground_shore_rgb
                         end_rgb = ground_high_rgb
                     else:
@@ -87,16 +103,16 @@ def main():
     p.add('-x', '--xSize', type=check_positive_integer)
     p.add('-y', '--ySize', type=check_positive_integer)
     p.add('--sea_level', type=float, default=0.0)
-    p.add('--seed', type=int, default=randint(0, 10000))
-    p.add('-s', '--scale', type=check_positive_integer, default=100)
-    p.add('-s2', '--scale2', type=check_positive_integer, default=50)
+    p.add('--seed', type=int, default=random.randint(0, 10000))
+    p.add('-f', '--filters', type=scale_pair)
     p.add('--sea_deep', type=check_hex)
     p.add('--sea_shore', type=check_hex)
     p.add('--ground_shore', type=check_hex)
     p.add('--ground_high', type=check_hex)
 
     args = p.parse_args()
-    map = Map(size_x=args.xSize, size_y=args.ySize, seed=args.seed, scale=args.scale, scale2=args.scale2)
+    print(args.seed)
+    map = Map(size_x=args.xSize, size_y=args.ySize, seed=args.seed, filters=args.filters)
     map.show_self(args.sea_deep, args.sea_shore, args.ground_shore, args.ground_high, args.sea_level)
 
 
