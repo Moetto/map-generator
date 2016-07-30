785a1dd10d59b64cc1b3939bd79c2f4113fa67d6
Antti Möttönen <antti.moettoenen@gmail.com>
2016-07-30 10:40:51 +0300

    Added GUI

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from noise import snoise2
 import numpy as np
 import random
-from PIL import Image
+from PIL import Image, ImageTk
 import configargparse
 import re
 from webcolors import hex_to_rgb
 from scipy.ndimage.morphology import binary_erosion
+import tkinter as tk
+from tkinter import ttk
 
 
 def check_positive_integer(value):
@@ -44,6 +46,99 @@ class _ScaleEffectPair:
         return "{} / {}".format(self.scale, self.effect)
 
 
+class UI(ttk.Frame):
+    def __init__(self, parent, args):
+        super().__init__(parent, padding="3 3 12 12")
+        self.map = None
+        self.args = args
+        self.controls = ttk.Frame(self)
+        self.controls.grid(column=0, row=0, sticky=(tk.W, tk.N))
+
+        self.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+        self.columnconfigure(0, weight=0)
+        self.columnconfigure(1, weight=0)
+        self.columnconfigure(2, weight=1)
+
+        seed_label = tk.Label(self.controls, text="Seed")
+        seed_label.grid(column=0, row=0, sticky=(tk.W, tk.N))
+        self.seed_value = tk.StringVar(value=args.seed)
+        self.entry_seed = ttk.Entry(self.controls, textvariable=self.seed_value)
+        self.entry_seed.grid(column=1, row=0, sticky=(tk.W, tk.N))
+        random_seed_label = tk.Label(self.controls, text="Random seed")
+        random_seed_label.grid(column=0, row=1, sticky=(tk.W, tk.N))
+        self.random_seed = tk.BooleanVar(value=False)
+        random_seed_button = tk.Checkbutton(self.controls, command=self.random_seed_action, variable=self.random_seed)
+        random_seed_button.grid(column=1, row=1, sticky=(tk.W, tk.N))
+
+        vno = parent.register(self._validate_number_only)
+        width_label = tk.Label(self.controls, text="Width")
+        width_label.grid(column=0, row=2, sticky=(tk.W, tk.N))
+        self.x_value = tk.IntVar(value=args.xSize)
+        entry_x = ttk.Entry(self.controls, textvariable=self.x_value, validate='all', validatecommand=(vno, '%P'),
+                            width=7)
+        entry_x.grid(column=1, row=2)
+
+        height_label = tk.Label(self.controls, text="Height")
+        height_label.grid(column=0, row=3, sticky=(tk.W, tk.N))
+        self.y_value = tk.IntVar(value=args.ySize)
+        entry_y = ttk.Entry(self.controls, textvariable=self.y_value, validate='all', validatecommand=(vno, '%P'),
+                            width=7)
+        entry_y.grid(column=1, row=3)
+
+        sea_label = tk.Label(self.controls, text="Sea level")
+        sea_label.grid(column=0, row=4, sticky=(tk.W, tk.N))
+        self.sea_level_value = tk.DoubleVar(value=args.sea_level)
+        sea_level_slider = ttk.Scale(self.controls, from_=0, to=100, variable=self.sea_level_value,
+                                     command=self.show_sea_level, orient="horizontal")
+        sea_level_slider.grid(column=1, row=4)
+        self.sea_level_display = tk.Label(self.controls, text=args.sea_level)
+        self.sea_level_display.grid(column=0, row=4, sticky=(tk.E, tk.N))
+
+        ttk.Button(self.controls, text="Generate height map", command=self.generate_height_map).grid(column=0, row=5)
+        ttk.Button(self.controls, text="Generate map", command=self.generate_map).grid(column=1, row=5)
+
+    def generate_map(self):
+        self._generate_map()
+        self.map.generate_map_image(self.args.sea_deep, self.args.sea_shore, self.args.ground_shore,
+                                    self.args.ground_high)
+        self._show_image(self.map.image)
+
+    def generate_height_map(self):
+        self._generate_map()
+        self.map.generate_height_map_image()
+        self._show_image(self.map.image)
+
+    def _generate_map(self):
+        if self.random_seed.get():
+            self.seed_value.set(random.randint(0, 10000))
+
+        self.map = Map(self.x_value.get(), self.y_value.get(), sea_level=int(self.sea_level_value.get()),
+                       seed=self.seed_value.get(),
+                       filters=self.args.filters)
+
+    def _show_image(self, image):
+        photo = ImageTk.PhotoImage(image)
+        map_label = tk.Label(self, image=photo)
+        map_label.image = photo
+        map_label.grid(column=2, row=0)
+
+    def random_seed_action(self):
+        if self.random_seed.get():
+            self.entry_seed.configure(state='disabled')
+        else:
+            self.entry_seed.configure(state='normal')
+
+    @staticmethod
+    def _validate_number_only(value):
+        regex = re.compile("^\d+$")
+        if regex.match(value):
+            return True
+        return False
+
+    def show_sea_level(self, value):
+        self.sea_level_display['text'] = int(float(value))
+
+
 class Map:
     def __init__(self, size_x, size_y, filters, sea_level, seed=10):
         self.size_x = size_x
@@ -51,6 +146,7 @@ class Map:
         self.grid = np.zeros((size_y, size_x), np.float32)
         self.theoretical_max_height = (sum([pair.effect for pair in filters])) * 2
         self.sea_level = sea_level
+        self.image = None
 
         random.seed(seed)
         offset = random.randint(0, 10000)
@@ -63,8 +159,8 @@ class Map:
                     height += snoise2((x + offset) / pair.scale, (y + offset) / pair.scale, 1) * pair.effect
 
                 if height > sea_level:
-                    height += abs(
-                        snoise2((x + offset) / mountain_scale, (y + offset) / mountain_scale, 1) * mountain_range)
+                    height += abs(snoise2((x + offset) / mountain_scale,
+                                          (y + offset) / mountain_scale, 1) * mountain_range)
 
                 self.grid[y, x] = height
         self.effective_sea_level = np.max(self.grid) * self.sea_level / 100
@@ -96,19 +192,16 @@ class Map:
     def get_continents(self):
         mask = np.zeros_like(self.grid)
         mask[self.grid > self.sea_level / 100 * np.max(self.grid)] = 1
-        Map.show_height_map(mask)
         return mask
 
-    @staticmethod
-    def show_height_map(grid):
-        max_height = np.max(grid)
-        height_map = np.copy(grid)
+    def generate_height_map_image(self):
+        max_height = np.max(self.grid)
+        height_map = np.copy(self.grid)
         for pixel in np.nditer(height_map, op_flags=['readwrite']):
             pixel[...] = 255 * pixel / max_height
-        image = Image.fromarray(height_map, "F")
-        image.show()
+        self.image = Image.fromarray(height_map, "F")
 
-    def show_self(self, sea_deep, sea_shore, ground_shore, ground_high):
+    def generate_map_image(self, sea_deep, sea_shore, ground_shore, ground_high):
         sea_deep_rgb = hex_to_rgb(sea_deep)
         sea_shore_rgb = hex_to_rgb(sea_shore)
         ground_shore_rgb = hex_to_rgb(ground_shore)
@@ -135,11 +228,10 @@ class Map:
                 for i in range(3):
                     rgb[i][y, x] = self._calculate_gradient_value(val, start_value, end_value, start_rgb, end_rgb, i)
 
-        image = Image.merge("RGB", (
+        self.image = Image.merge("RGB", (
             Image.fromarray(rgb[0], "L"),
             Image.fromarray(rgb[1], "L"),
             Image.fromarray(rgb[2], "L")))
-        image.show()
 
 
 def main():
@@ -148,7 +240,7 @@ def main():
     p.add('-x', '--xSize', type=check_positive_integer)
     p.add('-y', '--ySize', type=check_positive_integer)
     p.add('--sea_level', type=int, default=75, help="Percentage of max height below which area is covered in water")
-    p.add('--seed', type=int, default=random.randint(0, 10000))
+    p.add('--seed', default=random.randint(0, 10000))
     p.add('-f', '--filters', type=scale_pair)
     p.add('--sea_deep', type=check_hex)
     p.add('--sea_shore', type=check_hex)
@@ -156,11 +248,12 @@ def main():
     p.add('--ground_high', type=check_hex)
 
     args = p.parse_args()
-    print(args.seed)
-    map = Map(size_x=args.xSize, size_y=args.ySize, sea_level=args.sea_level, seed=args.seed, filters=args.filters)
-    map.generate_mountain(map.get_continents())
-    map.show_self(args.sea_deep, args.sea_shore, args.ground_shore, args.ground_high)
-    # map.show_height_map(map.grid)
+
+    root_window = tk.Tk()
+    root_window.title("Random map generator")
+    ui = UI(root_window, args)
+    ui.grid()
+    root_window.mainloop()
 
 
 if __name__ == "__main__":
